3b0b978d92096ea1321b6179fa0be9100197b590
Terry Morrow <terrymorrow@gmail.com>
2011-11-14 20:05:43 -0500

    updated parser, added styling

--- parser.rb
+++ parser.rb
@@ -8,7 +8,7 @@ files.each do |path|
   f = File.open(path)
   doc = Nokogiri::HTML(f)
   f.close
-  text = doc.css("#pageText").inner_html
+  text = doc.css("#pageText")
   f = File.open (path.gsub '/full','/partial'), "w+"
   f.write text
   f.close
